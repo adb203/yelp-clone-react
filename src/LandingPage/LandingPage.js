e4eaad8e3a2ccff8fdde398e917d2029e1bdd262
Ariandaniel Baoas <adb203@scarletmail.rutgers.edu>
2020-03-27 14:00:02 -0400

    Search bar

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {TopNav} from './TopNav/TopNav.js';
 import logo from '../assets/logo.png'
 import styles from './LandingPage.module.css'
+import {SearchBar} from '../SearchBar/SearchBar.js';
 
 export function LandingPage() {
   return (
     <div>
       <TopNav/>
       <img src={logo} className={styles.logo} alt='logo'/>
+      <SearchBar/>
     </div>
   );
 }
