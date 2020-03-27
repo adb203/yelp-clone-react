6d5e60721bc1785aaede1af8302815d3c43c447d
Ariandaniel Baoas <adb203@scarletmail.rutgers.edu>
2020-03-27 15:18:36 -0400

    react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { LandingPage } from './LandingPage/LandingPage.js';
+import {Route, Switch} from 'react-router-dom';
+import {Search} from './Search/Search.js';
 
 function App() {
   return (
-    <LandingPage/>
+    <Switch>
+      <Route path='/search' component={Search}/>
+      <Route path='/' component={LandingPage}/>
+    </Switch>
   );
 }
 
